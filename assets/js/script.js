e1e26c22845bbadd4641b759eb904a4181a58626
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-04-04 18:08:48 -0400

    updating files

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,22 +17,43 @@ function showClock() {
 
 
 //code to handle schedule event input
-removeBtnEl.on('click', function() {
-    location.reload(".row");
+removeBtnEl.on('click', function handleRemovedItem(event) {
+    var btnClicked = $(event.target);
+    location.reload();
+   localStorage.removeItem();
 });
 
 function handleEventInput(event) {
     //Prevent the default behavior
     event.preventDefault();
-    console.log('Current Day: ', currentDayEl.val());
-    console.log('Current Time: ', currentTimeEl.val());
+    console.log('Current Day: ' + currentDayEl.text());
+    console.log('Current Time: ', currentTimeEl.text());
     console.log('Scheduled: ', currentEventEl.val());
-       
+    
+    var activities = [];
+    function addActivity() {
+        var inp = document.getElementById('eventInput');
+        activities.push(inp.value);
+        inp.value="";
+    }
+    
+    localStorage.setItem("activities", JSON.stringify(activities));
+
 }
 saveBtnEl.on('click', handleEventInput);
 
-
-
+//color rows
+var hr = (new Date()).getHours();
+var startTime = 8;
+for (var i = 0; i <= 10; i++){
+    var rowi = document.querySelector("#row"+i);
+    if (hr >= startTime+i && hr < startTime+i+1){
+        rowi.toggleClass('.past');
+    }else if (hr < startTime+i+1){
+        rowi.toggleClass(".present");
+    } else {rowi.toggleClass(".future");
+        }
+}
 
 
 
