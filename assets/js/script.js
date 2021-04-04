a330b95ad3fd8e2304ab3f64e5395ed089f15212
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-04-04 00:26:58 -0400

    updating repos with files

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//Task1: when the planner is opened the current day & time is displayed
+var currentDayEl = $('#currentDay');
+var currentTimeEl = $('.currentTime');
+var currentEventEl = $('#eventInput');
+
+function showDay() {
+    var today = moment().format('dddd');
+    currentDayEl.text(today);
+} setInterval(showDay, 500);
+
+function showClock() {
+    var TicToc = moment().format('MMM DD, YYYY hh:mm:ssA');
+    currentTimeEl.text(TicToc);
+} setInterval(showClock, 500);
+
+
+//code to handle schedule event input
+function scheduleEvent(event){
+    event.preventDefault();
+
+    var eventItem = $('input[name="event-input"]').val();
+
+    //show event
+    currentEventEl.append(eventItem);
+}
+
+
+ 
