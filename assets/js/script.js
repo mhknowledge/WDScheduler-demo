0de4b32d3aba56a05be64cb4aaba5b52313914e2
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-04-04 12:17:12 -0400

    changes to js file...removebtn...

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,25 +15,70 @@ function showClock() {
 
 
 //code to handle schedule event input
-    // var eventItem = $('input[name="event-input"]').val();
-    var saveInput = [];
+    
+    $('input').on("blur", function(){
+        $(this).val(function(i, val) {
+            
+            return val.toUpperCase();
+        });
+        
+    });
+$('input').ready(function(){
+    $('removeBtn').click(function(){
+        /*clear all input type*/
+        $('event-input input[type="text"]' ).val('');
+    });
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function removeElement(removeBtn){
+    //     var element = document.getElementbyId();
+    //     element.parentNode.removechild(element);
+    // }
+    
+    // $('input').on("blur", function(){
+    //     $(this).val(function(i, val){
+
+    //     return val.;
+    // });
+    // });
+    // $('removeBtn').addEventListener('click', removeElement("input") );
+
+
+
+
+    // var eventItem = $('event-input').val();
+    // let saveInput = JSON.stringify(input);
+
+    // var input = document.getElementById("event-input");
+    
+    
+    
+    // localStorage.setItem("input", saveInput);
+    // console.log(localStorage);
     // //show event
     // currentEventEl.append(eventItem);
     
-    var input = document.getElementById("event-input");
-    $('.saveBtn').click(function () {
-    saveInput.push({
-        eventItem: "",
-        })
-        localStorage.setItem("event", JSON.stringify(saveInput));
-    });
-    
-
-
-
-
-
-
-
- 
+   
+    // $('.saveBtn').click(function () {
+    // saveInput.push({
+    //     item = "input",
+    //     time: "TicToc",
+    //     })
+    //     localStorage.setItem("event", JSON.stringify(saveInput));
+    // });
