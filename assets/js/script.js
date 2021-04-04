a9cf9023caadea0e6a3a3cb6f033cf0ce097c738
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-04-04 15:34:50 -0400

    update to remove button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var currentDayEl = $('#currentDay');
 var currentTimeEl = $('.currentTime');
 var currentEventEl = $('#eventInput');
-
+var removeBtnEl = $('.removeBtn');
+var saveBtnEl = $('.saveBtn');
 function showDay() {
     var today = moment().format('dddd');
     currentDayEl.text(today);
@@ -14,20 +15,10 @@ function showClock() {
 } setInterval(showClock, 500);
 
 
+
 //code to handle schedule event input
-    
-    $('input').on("blur", function(){
-        $(this).val(function(i, val) {
-            
-            return val.toUpperCase();
-        });
-        
-    });
-$('input').ready(function(){
-    $('removeBtn').click(function(){
-        /*clear all input type*/
-        $('event-input input[type="text"]' ).val('');
-    });
+removeBtnEl.on('click', function() {
+    location.reload(".row");
 });
 
 
@@ -45,40 +36,3 @@ $('input').ready(function(){
 
 
 
-
-    // function removeElement(removeBtn){
-    //     var element = document.getElementbyId();
-    //     element.parentNode.removechild(element);
-    // }
-    
-    // $('input').on("blur", function(){
-    //     $(this).val(function(i, val){
-
-    //     return val.;
-    // });
-    // });
-    // $('removeBtn').addEventListener('click', removeElement("input") );
-
-
-
-
-    // var eventItem = $('event-input').val();
-    // let saveInput = JSON.stringify(input);
-
-    // var input = document.getElementById("event-input");
-    
-    
-    
-    // localStorage.setItem("input", saveInput);
-    // console.log(localStorage);
-    // //show event
-    // currentEventEl.append(eventItem);
-    
-   
-    // $('.saveBtn').click(function () {
-    // saveInput.push({
-    //     item = "input",
-    //     time: "TicToc",
-    //     })
-    //     localStorage.setItem("event", JSON.stringify(saveInput));
-    // });
