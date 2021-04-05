5903cf1c06a4852a5fbe56fd614b884e017e0021
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-04-05 08:24:27 -0400

    attempts to fix console issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //Task1: when the planner is opened the current day & time is displayed
 var currentDayEl = $('#currentDay');
 var currentTimeEl = $('.currentTime');
-var currentEventEl = $('input[type="text"]');
+var currentEventEl = document.getElementsByName('eventInput[]');
 var removeBtnEl = $('.removeBtn');
 var saveBtnEl = $('.saveBtn');
 
@@ -36,24 +36,17 @@ function handleEventInput(event) {
     console.log('Current Time: ', currentTimeEl.text());
     console.log('Scheduled: ', currentEventEl);
     
-    var activities = [];
+    
     
     function addActivity() {
-        var inp0 = document.getElementById('eventInput0');
-        var inp1 = document.getElementById('eventInput1');
-        var inp2 = document.getElementById('eventInput2');
-        var inp3= document.getElementById('eventInput3');
-        var inp4 = document.getElementById('eventInput4');
-        var inp5 = document.getElementById('eventInput5');
-        var inp6 = document.getElementById('eventInput6');
-        var inp7 = document.getElementById('eventInput7');
-        var inp8 = document.getElementById('eventInput8');
-        var inp9 = document.getElementById('eventInput9');
-        activities.push('inp0','inp1','inp2','inp3','inp4','inp5','inp6','inp7', 'inp8','inp9'); 
-        console.table(activities);
+        
+        localStorage.setItem("currentEventEl", JSON.stringify(currentEventEl));
+        
+    
+        
        
     }
-    localStorage.setItem("activities", JSON.stringify(activities));
+    
 
 }
 
@@ -62,19 +55,19 @@ function handleEventInput(event) {
 
 
 //color code rows
-// var hr = (new Date()).getHours();
-// console.log(hr);
-// var startTime = 8;
-// for (var i = 0; i <= 12; i++){
-//     var rowEl = document.querySelector("#row"+i);
-//     if (hr >= startTime+i && hr < startTime+i+1){
-//         rowEl.toggleClass('.past');
-//     }else if (hr < startTime+i+1){
-//         rowEl.toggleClass(".present");
-//     } else if (hr > startTime ) {
-//         rowEl.toggleClass(".future");
-//         }
-// }
+var hr = (new Date()).getHours();
+console.log("It is now: " + hr);
+var startTime = 8;
+for (var i = 0; i <= 12; i++){
+    var rowEl = document.querySelector("#row"+i);
+    if (hr >= startTime+i && hr < startTime+i+1){
+        rowEl.toggleClass('.past');
+    }else if (hr < startTime+i+1){
+        rowEl.toggleClass(".present");
+    } else if (hr > startTime ) {
+        rowEl.toggleClass(".future");
+        }
+}
 
 
 
